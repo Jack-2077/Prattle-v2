beb78801018db3d1197ba4dfc9133bffad193cc4
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-03-30 08:58:45 -0700

    feat: implement auth0 login authentication

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { AuthModule } from '@auth0/auth0-angular';
+import { AuthButtonComponent } from './auth-button/auth-button.component';
+
+@NgModule({
+  declarations: [AppComponent, AuthButtonComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AuthModule.forRoot({
+      domain: 'dev-cystsk94.us.auth0.com',
+      clientId: 'jgyBuUkSsBchlFjuBUSsgUw37VQskL1Q',
+    }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
