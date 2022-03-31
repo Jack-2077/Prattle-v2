db526cf6b15075591e8d9f84a334450a958537bb
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-03-31 08:40:57 -0700

    feat: change css to scss stylesheet

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CovalentLayoutModule } from '@covalent/core/layout';
+
 import { AuthModule } from '@auth0/auth0-angular';
 import { AuthButtonComponent } from './auth-button/auth-button.component';
 
 @NgModule({
   declarations: [AppComponent, AuthButtonComponent],
   imports: [
+    CovalentLayoutModule,
     BrowserModule,
     AppRoutingModule,
     AuthModule.forRoot({
