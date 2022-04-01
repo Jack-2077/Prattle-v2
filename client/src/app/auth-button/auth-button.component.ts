8c2e61b8f1549278f9b51f2581bcc634f3836400
Jack <78120697+Jack-2077@users.noreply.github.com>
2022-03-31 19:16:50 -0700

    fix: log out button not displaying after login

--- client/src/app/auth-button/auth-button.component.ts
+++ client/src/app/auth-button/auth-button.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-auth-button',
   templateUrl: './auth-button.component.html',
   styleUrls: ['./auth-button.component.scss'],
 })
-export class AuthButtonComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class AuthButtonComponent {
+  constructor(
+    @Inject(DOCUMENT) public document: Document,
+    public auth: AuthService
+  ) {}
 }
