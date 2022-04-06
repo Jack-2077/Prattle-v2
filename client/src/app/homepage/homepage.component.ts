ff96134b01db80b39cf4fb84a7285c749ae2d7c4
Cezaraus <brianopao@gmail.com>
2022-04-06 01:36:35 -0700

    feat: attempt display user list
    Co-authored-by: Jack <Jack-2077@users.noreply.github.com>

--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
 import { Socket } from 'ngx-socket-io';
+import { map } from 'rxjs';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.scss'],
 })
+
 export class HomepageComponent implements OnInit {
+  @Input() userList: any;
+  @Input() USER: any;
+
   constructor(public auth: AuthService, public socket: Socket) {
+
     let authArray = [];
-    let userName, userEmail;
+    let userName;
     this.auth.user$.forEach((val) => authArray.push(val));
 
     const myTimeout = setTimeout(addUser, 1000);
@@ -25,10 +31,37 @@ export class HomepageComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void {}
+
+  ngOnInit(): void {
+    this.userList = this.getUsers();
+
+    const myTimeout = setTimeout(list, 1000);
+    function list() {
+      console.log(this.userList)
+      this.USER = this.userList[0];
+    }
+
+
+
+
+    //set
+  }
 
   display = false;
   onPress() {
     this.display = !this.display;
   }
+
+
+
+  getUsers() {
+    let userList = [];
+    let users;
+    let something;
+    users = this.socket.fromEvent('current_users').pipe(map((data: any) => data));
+    users.forEach(val => userList.push(Object.values(val)));
+
+  }
+
+
 }
